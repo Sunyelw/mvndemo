9f0d102914988ca74d310f2f7a312584bc0f03cb
huangyang3 <huangyang3@asiainfo.com>
2019-11-04 15:11:05 +0800

    project init

---
+++ src/main/java/com/sun/yelw/HyAnnotationGoal.java
@@ -0,0 +1,32 @@
+package com.sun.yelw;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+/**
+ * 项目名称:   pinkstone
+ * 包:        com.sun.yelw
+ * 类名称:     HyAnnotationGoal
+ * 类描述:     注解方式 自定义 maven 目标，并绑定到 maven 插件
+ * 创建人:     huangyang
+ * 创建时间:   2019/11/4 14:31
+ */
+@Mojo(name = "hy-an", defaultPhase = LifecyclePhase.COMPILE)
+public class HyAnnotationGoal extends AbstractMojo {
+
+    @Parameter
+    private String age;
+
+    @Parameter
+    private String name;
+
+    public void execute() throws MojoExecutionException, MojoFailureException{
+        getLog().error("name: " + name);
+        getLog().error("hy-plugin-execute-by-annotation");
+        getLog().error("age: " + age);
+    }
+}
---
+++ src/main/java/com/sun/yelw/HyDocGoal.java
@@ -0,0 +1,39 @@
+package com.sun.yelw;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * 包:        com.sun.yelw
+ * 类名称:     HyGoal
+ * 类描述:     文档方式 自定义 maven 目标， 在文档注释中添加注解 @goal @parameter
+ * 创建人:     huangyang
+ * 创建时间:   2019/11/4 13:37
+ *
+ * @goal hy-doc
+ */
+public class HyDocGoal extends AbstractMojo {
+
+    // 这种写法被废弃了
+    // @parameter expression="${name}"
+    /**
+     * @parameter property="name"
+     */
+    private String name;
+
+    // @parameter expression="${age}"
+    /**
+     * @parameter property="age"
+     */
+    private String age;
+
+    public void execute() throws MojoExecutionException, MojoFailureException{
+
+        getLog().error("++++++++++++++++++++");
+        getLog().error("hy-plugin-execute-by-document");
+        getLog().error("name: " + name + ", age: " + age);
+        getLog().error("++++++++++++++++++++");
+
+    }
+}
